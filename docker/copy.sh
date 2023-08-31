1c35c1a487aac7b7e93555191d0632f0e23c6994
zhangyijun <1533871521@qq.com>
2023-08-31 15:51:50 +0800

    docker deploy

---
+++ docker/copy.sh
@@ -0,0 +1,17 @@
+#gateway
+cp ../douyin-server ./gateway/
+
+#microservices
+BIN_DIR="../rpc/output/bin/*"
+for d in $BIN_DIR ; do
+  SVR_NAME="${d#$BIN_DIR}"
+  if [ ! -z "$1" ]; then
+    if [ $SVR_NAME != "$1" ]; then
+      continue
+    fi
+  fi
+  cp ../rpc/output/bin/$SVR_NAME ./microservices/$SVR_NAME
+done
+
+#mysql2redis
+cp ../database/mysql2redis ./mysql2redis/mysql2rediss
